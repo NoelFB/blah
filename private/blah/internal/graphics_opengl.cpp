53c65f0ba3979abe119a134fce4ef655c7329fe1
Noel Berry <thenberry@gmail.com>
2020-08-27 19:49:23 -0700

    line formatting

--- private/blah/internal/graphics_opengl.cpp
+++ private/blah/internal/graphics_opengl.cpp
@@ -19,24 +19,24 @@
 #define APIENTRY
 #endif
 
-typedef ptrdiff_t		GLintptr;
-typedef ptrdiff_t		GLsizeiptr;
+typedef ptrdiff_t	GLintptr;
+typedef ptrdiff_t	GLsizeiptr;
 typedef unsigned int	GLenum;
 typedef unsigned char	GLboolean;
 typedef unsigned int	GLbitfield;
-typedef void			GLvoid;
-typedef signed char		GLbyte;			/* 1-byte signed */
-typedef short			GLshort;		/* 2-byte signed */
-typedef int				GLint;			/* 4-byte signed */
-typedef unsigned char	GLubyte;		/* 1-byte unsigned */
-typedef unsigned short	GLushort;		/* 2-byte unsigned */
-typedef unsigned int	GLuint;			/* 4-byte unsigned */
-typedef int				GLsizei;		/* 4-byte signed */
-typedef float			GLfloat;		/* single precision float */
-typedef float			GLclampf;		/* single precision float in [0,1] */
-typedef double			GLdouble;		/* double precision float */
-typedef double			GLclampd;		/* double precision float in [0,1] */
-typedef char			GLchar;
+typedef void		GLvoid;
+typedef signed char	GLbyte;		/* 1-byte signed */
+typedef short		GLshort;	/* 2-byte signed */
+typedef int		GLint;		/* 4-byte signed */
+typedef unsigned char	GLubyte;	/* 1-byte unsigned */
+typedef unsigned short	GLushort;	/* 2-byte unsigned */
+typedef unsigned int	GLuint;		/* 4-byte unsigned */
+typedef int		GLsizei;	/* 4-byte signed */
+typedef float		GLfloat;	/* single precision float */
+typedef float		GLclampf;	/* single precision float in [0,1] */
+typedef double		GLdouble;	/* double precision float */
+typedef double		GLclampd;	/* double precision float in [0,1] */
+typedef char		GLchar;
 
 #define GL_DONT_CARE 0x1100
 #define GL_ZERO 0x0000
