47555a51cbeedf327c1dbffba264c656bad07fbe
Noel Berry <thenberry@gmail.com>
2021-01-04 19:01:08 -0800

    D3D11 BlendMode Mask wasn't hooked up

--- src/internal/graphics_backend_d3d11.cpp
+++ src/internal/graphics_backend_d3d11.cpp
@@ -1338,7 +1338,15 @@ namespace Blah
 			blend.alpha_src == BlendFactor::One && blend.alpha_dst == BlendFactor::Zero
 			);
 
-		desc.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
+		desc.RenderTarget[0].RenderTargetWriteMask = 0;
+		if (((int)blend.mask & (int)BlendMask::Red) == (int)BlendMask::Red)
+			desc.RenderTarget[0].RenderTargetWriteMask |= D3D11_COLOR_WRITE_ENABLE_RED;
+		if (((int)blend.mask & (int)BlendMask::Green) == (int)BlendMask::Green)
+			desc.RenderTarget[0].RenderTargetWriteMask |= D3D11_COLOR_WRITE_ENABLE_GREEN;
+		if (((int)blend.mask & (int)BlendMask::Blue) == (int)BlendMask::Blue)
+			desc.RenderTarget[0].RenderTargetWriteMask |= D3D11_COLOR_WRITE_ENABLE_BLUE;
+		if (((int)blend.mask & (int)BlendMask::Alpha) == (int)BlendMask::Alpha)
+			desc.RenderTarget[0].RenderTargetWriteMask |= D3D11_COLOR_WRITE_ENABLE_ALPHA;
 
 		if (desc.RenderTarget[0].BlendEnable)
 		{
